2df517a6ee4a75fd59f59146b2aebfd7197c62d8
nmishra-ufl <nmishra@ufl.edu>
2023-03-22 20:49:54 -0400

    added decode() and edited main() to fit functionality of decode()

--- main.py
+++ main.py
@@ -6,7 +6,15 @@ def encode(password):
     final_password = "".join(encoded_string)
     return final_password
 
+def decode(password):
+    # written by Nikhil Mishra
+    decoded_pass = ""
+    for num in password:
+        decoded_pass += (str(abs((int(num) - 3) % 10)))
+    return decoded_pass
+
 def main():
+    encoded_pass = ""
     while True:
         print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
         menu_option = int(input("\nPlease enter an option: "))
@@ -15,7 +23,7 @@ def main():
             encoded_pass = encode(user_input)
             print("Your password has been encoded and stored!\n")
         if menu_option == 2:
-            print(f'The encoded password is {encoded_pass}, and the original password is {user_input}.\n')
+            print(f'The encoded password is {encoded_pass}, and the decoded password is {decode(encoded_pass)}.\n')
         if menu_option == 3:
             break
 
